7942f16095922c9e3c57789eb3bebcc46f1c1ea4
Yuga Ego <yugenekr@gmail.com>
2020-02-13 19:44:07 +0200

    Move exercises to 01-Arithmetic directory

--- 01-Fixed-Size-Data/Ex2.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; Definitions
-(define prefix "hello")
-(define suffix "world")
-
-; Application
-(string-append prefix "_" suffix)
