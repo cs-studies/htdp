5286453657a180c291657cf9f64e00db1a789125
Yuga Ego <yugenekr@gmail.com>
2020-02-15 23:20:00 +0200

    Add exercise 20

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-20.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-20) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Exercise 20. Define the function string-delete, which consumes a string plus a number i and deletes the ith position from str. Assume i is a number between 0 (inclusive) and the length of the given string (exclusive). See exercise 4 for ideas. Can string-delete deal with empty strings?
+|#
+
+; Functions
+(define (string-delete str i)
+  (if (> (string-length str) 0)
+      (string-append (substring str 0 i) (substring str (+ i 1)))
+      ""))
+
+; Application
+(string-delete "Funny_Faces" 5)
