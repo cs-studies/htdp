83f40fb175451bff03e88d8ad6848eca4d36dc41
Yuga Ego <yugenekr@gmail.com>
2020-02-15 21:59:17 +0200

    Add exercise 15

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-15.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-15) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Exercise 15. Define ==>. The function consumes two Boolean values, call them sunny and friday. Its answer is #true if sunny is false or friday is true.
+|#
+
+; Functions
+(define (==> sunny friday)
+  (or (not sunny) friday))
