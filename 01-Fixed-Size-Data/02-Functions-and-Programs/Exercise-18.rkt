28320fd4377e0ac809a5278e5987905ea34c726c
Yuga Ego <yugenekr@gmail.com>
2020-02-15 22:46:33 +0200

    Add exercise 18

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-18.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-18) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Exercise 18. Define the function string-join, which consumes two strings and appends them with "_" in between.
+|#
+
+; Functions
+(define (string-join str1 str2)
+  (string-append str1 "_" str2))
+
+; Application Example
+(string-join "Funny" "Faces")
+  
