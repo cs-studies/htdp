55c23497cb1765eedc9ec67d1ceffb401946fb15
Yuga Ego <yugenekr@gmail.com>
2020-02-15 20:45:41 +0200

    Add language declaration to exercise 12 file

--- 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-12.rkt
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-12.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-12) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Exercise 12. Define the function cvolume, which accepts the length of a side of an equilateral cube and computes its volume. If you have time, consider defining csurface, too.
 |#
