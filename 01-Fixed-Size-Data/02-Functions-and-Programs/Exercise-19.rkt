4a7dc1cca5c71d4187aad2c4ad63ec0af063bcc9
Yuga Ego <yugenekr@gmail.com>
2020-02-15 23:04:32 +0200

    Add exercise 19

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-19.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-19) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Exercise 19. Define the function string-insert, which consumes a string str plus a number i and inserts "_" at the ith position of str. Assume i is a number between 0 and the length of the given string (inclusive). See exercise 3 for ideas. Ponder how string-insert copes with "".
+|#
+
+; Functions
+(define (string-insert str i)
+  (string-append (substring str 0 i) "_" (substring str i)))
+
+; Application
+(string-insert "FunnyFaces" 5)
+(string-insert "" 0)
+  
