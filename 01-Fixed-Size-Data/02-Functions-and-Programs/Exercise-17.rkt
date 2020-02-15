b85bb5ed27007312971fe149ecc7657d8719f66f
Yuga Ego <yugenekr@gmail.com>
2020-02-15 22:41:32 +0200

    Add exercise 17

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-17.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-17) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Exercise 17. Define the function image-classify, which consumes an image and conditionally produces "tall" if the image is taller than wide, "wide" if it is wider than tall, or "square" if its width and height are the same.
+|#
+
+(require 2htdp/image)
+
+; Functions
+(define (image-classify img)
+  (if (> (image-height img) (image-width img))
+      "tall"
+      (if (> (image-width img) (image-height img))
+          "wide"
+          "square")))
