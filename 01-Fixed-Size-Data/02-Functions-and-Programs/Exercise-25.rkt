e25397e98a924af7ab4320f8b47b9e1cdf4becdf
Yuga Ego <yugenekr@gmail.com>
2020-02-17 12:48:20 +0200

    Add exercise 25

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-25.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-25) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 25.
+;; Take a look at this attempt to solve exercise 17:
+;; <code follows>
+;; Does stepping through an application suggest a fix?
+
+(require 2htdp/image)
+
+;; Exercise code
+(define (image-classify img)
+  (cond
+    [(>= (image-height img) (image-width img)) "tall"]
+    [(= (image-height img) (image-width img)) "square"]
+    [(<= (image-height img) (image-width img)) "wide"]))
+
+;; Square is not classified as "square"
+(image-classify (square 20 "outline" "blue"))
