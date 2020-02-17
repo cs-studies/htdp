92063293a54ee86f3e87a422549557cde709ecaf
Yuga Ego <yugenekr@gmail.com>
2020-02-17 12:52:15 +0200

    Add exercise 26

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-26.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-26) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 26.
+;; What do you expect as the value of this program:
+;; <code follows>
+;; Confirm your expectation with DrRacket and its stepper.
+
+;; Exercise code
+(define (string-insert s i)
+  (string-append (substring s 0 i)
+                 "_"
+                 (substring s i)))
+ 
+(string-insert "helloworld" 6)
