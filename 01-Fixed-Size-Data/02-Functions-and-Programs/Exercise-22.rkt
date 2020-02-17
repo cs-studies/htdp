0081543b749ea4d4569a4ee017fb39b6d43c0c5a
Yuga Ego <yugenekr@gmail.com>
2020-02-17 12:18:28 +0200

    Add exercise 22

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-22.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-22) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 22.
+;; Use DrRacket’s stepper on this program fragment:
+;; (define (distance-to-origin x y)
+;; (+ (sqr x) (sqr y))))
+;; (distance-to-origin 3 4)
+;; Does the explanation match your intuition?
+
+(define (distance-to-origin x y)
+  (sqrt (+ (sqr x) (sqr y))))
+
+(distance-to-origin 3 4)
