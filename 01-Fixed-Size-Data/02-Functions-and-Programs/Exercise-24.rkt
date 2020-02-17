b426742e63fa170ba236d5ea69f0745f34a310a8
Yuga Ego <yugenekr@gmail.com>
2020-02-17 12:41:12 +0200

    Add exercise 24

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-24.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 24.
+;; Here is the definition of ==>
+;; (define (==> x y)
+;;   (or (not x) y))
+;; Use the stepper to determine the value of (==> #true #false).
+
+(define (==> x y)
+  (or (not x) y))
+
+(==> #true #false)
