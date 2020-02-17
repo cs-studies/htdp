61722f04a868161883d409efabe0dfd0dbf21b77
Yuga Ego <yugenekr@gmail.com>
2020-02-17 14:53:57 +0200

    Add exercise 28

---
+++ 01-Fixed-Size-Data/02-Functions-and-Programs/Exercise-28.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-28) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 28.
+;; Determine the potential profit for these ticket prices: $1, $2, $3, $4, and $5.
+;; Which price maximizes the profit of the movie theater?
+;; Determine the best ticket price to a dime.
+
+;; Answer
+;; See implementation in the Exercise-27.rkt file.
+;; ----------------------
+;; price ($) | profit ($)
+;; ----------------------
+;;   1       | 511.2
+;;   2       | 937.2
+;;   3       | 1063.2
+;;   4       | 889.2
+;;   5       | 415.2
+;; ----------------------
+;; $3 price maximizes the profit.
