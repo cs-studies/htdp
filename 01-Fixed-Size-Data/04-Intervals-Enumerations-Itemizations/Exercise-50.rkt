16b77a252737d6c0bf43b1b5d8115492cd65fab4
Yuzhana Ego <yugenekr@gmail.com>
2020-02-27 13:36:05 +0200

    Add exercise 50

---
+++ 01-Fixed-Size-Data/04-Intervals-Enumerations-Itemizations/Exercise-50.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-50) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 50.
+;; Copy and paste the traffic-light-next function definition.
+;; Add enough tests to make DrRacket happy.
+
+;; TrafficLight -> TrafficLight
+;; Yields the next state given current state s.
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
