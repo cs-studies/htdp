1adc554acc32478fada1f0b4e4b0f047dc40aa31
Yuzhana Ego <yugenekr@gmail.com>
2020-02-28 19:09:36 +0200

    Add exercise 52

---
+++ 01-Fixed-Size-Data/04-Intervals-Enumerations-Itemizations/Exercise-52.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-52) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 52.
+;; Which integers are contained in the four intervals above?
+
+;; Closed interval.
+;; [3, 5] = {3, 4, 5}
+
+;; Left-open interval.
+;; (3, 5] = {4, 5}
+
+;; Right-open interval.
+;; [3, 5) = {3, 4}
+
+;; Open interval.
+;; (3, 5) = {4}
