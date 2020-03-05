ec6ae47efdeb9f09032eca397c8f8864a8e3dd73
Yuzhana Ego <yugenekr@gmail.com>
2020-03-05 17:38:29 +0200

    Add exercise 69

---
+++ 01-Fixed-Size-Data/05-Adding-Structure/Exercise-69.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-69) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 69.
+;; Draw box representations for the solution of exercise 65.
+
+(require 2htdp/image)
+
+(bitmap "./images/diagrams.png")
