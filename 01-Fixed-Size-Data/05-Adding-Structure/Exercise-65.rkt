07d168f17a3a71dd5a00b44517a94e74d5a4ad83
Yuzhana Ego <yugenekr@gmail.com>
2020-03-05 02:09:46 +0200

    Add exercise 65

---
+++ 01-Fixed-Size-Data/05-Adding-Structure/Exercise-65.rkt
@@ -0,0 +1,92 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-65) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 65.
+;; Take a look at the following structure definitions.
+;; Write down the names of the functions
+;; (constructors, selectors, and predicates) that each introduces.
+
+
+;; Definition 1.
+(define-struct movie [title producer year])
+
+;; Constructor
+; make-movie
+(define m (make-movie "Some Title" "THE Producer" "2020"))
+
+;; Selectors
+(movie-title m)
+(movie-producer m)
+(movie-year m)
+
+;; Predicate
+(movie? m)
+
+
+;; Definition 2.
+(define-struct person [name hair eyes phone])
+
+;; Constructor
+; make-person
+(define p1 (make-person "Alan Turing" "Brown" "Blue" "21910532"))
+
+;; Selectors
+(person-name p1)
+(person-hair p1)
+(person-eyes p1)
+(person-phone p1)
+
+;; Predicate
+(person? p1)
+(person? m)
+
+
+;; Definition 3.
+(define-struct pet [name number])
+
+;; Constructor
+; make-pet
+(define p2 (make-pet "BumbleBee" 33))
+
+;; Selectors
+(pet-name p2)
+(pet-number p2)
+
+;; Predicate
+(pet? p2)
+(person? p2)
+
+
+;; Definition 4.
+(define-struct CD [artist title price])
+
+;; Constructor
+; make-CD
+(define c (make-CD "Maya P." "Dancing" 33.5))
+
+;; Selectors
+(CD-artist c)
+(CD-title c)
+(CD-price c)
+
+;; Predicate
+(CD? c)
+(CD? m)
+
+
+;; Definition 5.
+(define-struct sweater [material size producer])
+
+;; Constructor
+; make-sweater
+(define s (make-sweater "Wool" "M" "ZaphodB"))
+
+;; Selectors
+(sweater-material s)
+(sweater-size s)
+(sweater-producer s)
+
+;; Predicate
+(sweater? s)
+(sweater? c)
