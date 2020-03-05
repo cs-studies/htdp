6f44b9c4957bda72f59666da7064fe697e5890dc
Yuzhana Ego <yugenekr@gmail.com>
2020-03-05 15:49:32 +0200

    Add exercise 68

---
+++ 01-Fixed-Size-Data/05-Adding-Structure/Exercise-68.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-68) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 68.
+;; Create an instance of ballf that has the same interpretation as ball1.
+
+;(define ball1
+;  (make-ball (make-posn 30 40) (make-vel -10 5)))
+
+(define-struct ballf [x y deltax deltay])
+
+(make-ballf 30 40 -10 5)
