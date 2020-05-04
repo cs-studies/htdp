8f85fcaa324ccf88e2327b97e7e78e46bfe6ca82
Y.E <yugenekr@gmail.com>
2020-05-04 12:46:31 +0300

    Add exercise 129

---
+++ 02-Arbitrarily-Large-Data/08-Lists/Exercise-129.rkt
@@ -0,0 +1,41 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-129) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 129.
+;; Create BSL lists that represent:
+
+
+;; a list of celestial bodies, say, at least all the planets in our solar system
+(cons "Neptune"
+      (cons "Uranus"
+            (cons "Saturn"
+                  (cons "Jupiter"
+                        (cons "Mars"
+                              (cons "Earth"
+                                    (cons "Venus"
+                                          (cons "Mercury"
+                                                '()))))))))
+
+;; a list of items for a meal
+(cons "Mandarin"
+      (cons "Apple"
+            (cons "Coconut"
+                  (cons "Honey"
+                        (cons "Cinnamon"
+                              (cons "Cardamom"
+                                    '()))))))
+
+;; a list of colors
+(cons "red"
+      (cons "green"
+            (cons "blue"
+                  '())))
+
+
+;;; Question
+;; Which of the sketches do you like better? 
+
+;;; Answer
+;; The sketches that list the boxes in the order in which they are consed together.
+
