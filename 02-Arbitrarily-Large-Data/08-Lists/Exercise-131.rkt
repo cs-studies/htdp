09b42a45cfb3b02e0f1c523a25066b009eaa9781
Y.E <yugenekr@gmail.com>
2020-05-04 13:13:01 +0300

    Add exercise 131

---
+++ 02-Arbitrarily-Large-Data/08-Lists/Exercise-131.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-131) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 131.
+;; Provide a data definition for representing lists of Boolean values.
+;; The class contains all arbitrarily long lists of Booleans.
+
+
+;; A List-of-bools is one of: 
+;; – '()
+;; – (cons Boolean List-of-bools)
+
+
+;; Example
+(cons #false
+      (cons #true
+            (cons #true '())))
+
