344b88c784f2aafcba3f82d740aef5fd10a9a7b4
Y.E <yugenekr@gmail.com>
2020-05-11 14:42:08 +0300

    Add exercise 148

---
+++ 02-Arbitrarily-Large-Data/09-Designing-with-Self-Referential-Data-Definitions/Exercise-148.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-148) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 148.
+;; Compare the function definitions from this section
+;; (sum, how-many, all-true, one-true)
+;; with the corresponding function definitions from the preceding sections.
+;; Is it better to work with data definitions
+;; that accommodate empty lists
+;; as opposed to definitions for non-empty lists?
+;; Why? Why not?
+
+
+;; Answer
+;; Data definitions that accommodate empty lists
+;; cover more use cases with a bit simpler definitions and functions implementation.
+;; Getting more at a lower cost usually is a preferred option.
+
