aca418bab6bac8660d41428cf76f3301b173ba26
Y.E <yugenekr@gmail.com>
2020-05-16 12:58:11 +0300

    Complement exercise 164

--- 02-Arbitrarily-Large-Data/10-More-on-Lists/Exercise-164.rkt
+++ 02-Arbitrarily-Large-Data/10-More-on-Lists/Exercise-164.rkt
@@ -4,6 +4,9 @@
 ;;
 ;; Exercise 164.
 ;; Design the function convert-euro, which converts a list of US$ amounts into a list of € amounts.
+;; Generalize convert-euro to the function convert-euro*,
+;; which consumes an exchange rate and a list of US$ amounts
+;; and converts the latter into a list of € amounts.
 
 
 ;; A List-of-numbers is one of:
@@ -11,6 +14,7 @@
 ;; - (cons Number List-of-numbers)
 
 (define RATE 0.9242) ; 1 USD = 0.9242 EUR
+
 
 ;; List-of-numbers -> List-of-numbers
 ;; Converts a list of USD amounts into a list of EUR amounts.
@@ -28,4 +32,15 @@
   (cond
     [(empty? usd) '()]
     [else (cons (* (first usd) RATE) (to-euro (rest usd)))]))
+
+
+;; List-of-numbers -> List-of-numbers
+;; Converts a list of USD amounts into a list of EUR amounts
+;; using an exchange rate r.
+(check-expect (to-euro* '() 0.9) '())
+(check-expect (to-euro* (cons 100 (cons 20 '())) 0.9) (cons 90 (cons 18 '())))
+(define (to-euro* usd r)
+  (cond
+    [(empty? usd) '()]
+    [else (cons (* (first usd) r) (to-euro* (rest usd) r))]))
 
