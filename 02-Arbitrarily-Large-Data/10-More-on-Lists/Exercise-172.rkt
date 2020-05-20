f831fe0f14832c8c185ae29e88ed8f7c928cd9ac
Y.E <yugenekr@gmail.com>
2020-05-20 08:20:35 +0300

    Add exercise 172

---
+++ 02-Arbitrarily-Large-Data/10-More-on-Lists/Exercise-172.rkt
@@ -0,0 +1,52 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-172) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 172.
+;; Design the function collapse, which converts a list of lines into a string.
+
+
+(require 2htdp/batch-io)
+
+;; An LLS is one of:
+;; – '()
+;; – (cons List-of-strings LLS)
+;; Represents a list of lines, each is a list of Strings.
+
+
+(define line0 (cons "hello" (cons "world" '())))
+(define line1 '())
+ 
+(define lls0 '())
+(define lls1 (cons line0 (cons line1 '())))
+(define lls2 (cons line0 (cons line0 '())))
+
+;; LLS -> String
+;; Converts a list of lines into a string.
+(check-expect (collapse lls0) "")
+(check-expect (collapse lls1) "hello world\n")
+(check-expect (collapse (cons line0 '())) "hello world\n")
+(check-expect (collapse lls2) "hello world\nhello world\n")
+(define (collapse lls)
+  (cond
+    [(empty? lls) ""]
+    [(cons? lls)
+     (string-append (collapse-line (first lls)) (collapse (rest lls)))]))
+
+;; List-of-strings -> String
+;; Counts the words on each line in the given file.
+(define (collapse-line ln)
+  (cond
+    [(empty? ln) ""]
+    [(cons? ln)     
+     (string-append (first ln)
+                    (if (empty? (rest ln)) "\n" " ")
+                    (collapse-line (rest ln)))]))
+
+
+;;; Application
+#|
+(write-file "files/ttt.dat"
+            (collapse (read-words/line "files/ttt.txt")))
+|#
+
