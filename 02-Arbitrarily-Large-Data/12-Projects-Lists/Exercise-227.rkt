00115cc1bbda66a3d307929e18ed06e74331d882
Y.E <yugenekr@gmail.com>
2020-06-27 16:38:03 +0300

    Add exercise 227

---
+++ 02-Arbitrarily-Large-Data/12-Projects-Lists/Exercise-227.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Exercise-227) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 227.
+;; The BW Machine is an FSM that flips
+;; from black to white and back to black for every key event.
+;; Formulate a data representation for the BW Machine.
+
+
+;; FSM-State is a Color.
+
+(define-struct transition [current next])
+;; A Transition is a structure:
+;;    (make-transition FSM-State FSM-State)
+
+;; An FSM is one of:
+;; - '()
+;; - (cons Transition FSM)
+;; Represents the transitions that a finite state machine
+;; can take from one state to another in reaction to keystrokes.
+
+
+(define BW-Machine
+  (list (make-transition "black" "white")
+        (make-transition "white" "black")))
+
