b5c7f73c39f7a6f2e996d34ae8ac4a98dbfca56a
Y.E <yugenekr@gmail.com>
2020-07-20 13:55:55 +0300

    Add exercise 321

---
+++ 04-Intertwined-Data/19-The-Poetry-of-S-expressions/Exercise-321.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise-321) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 321.
+;; Abstract the data definitions for S-expr and SL
+;; so that they abstract over the kinds of Atoms that may appear.
+
+
+;; An Atom is one of:
+;; – Number
+;; – String
+;; – Symbol
+
+;; An SL is one of:
+;; - [List-of Atom]
+;; - [List-of SL]
+
+;; An S-expr is one of:
+;; – Atom
+;; – SL
+
