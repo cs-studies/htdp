4f9a0522bf0e6c3afa1951958966454161d7766d
Y.E <yugenekr@gmail.com>
2020-07-25 09:09:19 +0300

    Add exercise 346

---
+++ 04-Intertwined-Data/21-Refining-Interpreters/Exercise-346.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise-346) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 346.
+;; Formulate a data definition for the class of values
+;; to which a representation of a BSL expression can evaluate.
+
+
+;;; Definition according to the chapter examples.
+;; A BSL-value is a Number.
+
+;;; A broader definition according to the Intermezzo 1.
+;; A BSL-value is one of:
+;; - Number
+;; - Boolean
+;; - String
+;; - Image
+
