205e67b5e4a3ec15d68688ec0dbbd2a3e0aff1c3
Y.E <yugenekr@gmail.com>
2020-09-01 18:00:31 +0300

    Add exercise 424

---
+++ 05-Generative-Recursion/25-Non-standard-Recursion/Exercise-424.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise-424) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 424.
+;; Draw a quick-sort diagram like the one in figure 148
+;; for (list 11 9 2 18 12 14 4 1).
+
+
+(require 2htdp/image)
+
+
+(bitmap "./images/quick-sort.png")
+
