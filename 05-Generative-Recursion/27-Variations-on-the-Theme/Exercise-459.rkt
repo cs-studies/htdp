ba149fb06582ec5da72e2b8546ea96d93209b513
Y.E <yugenekr@gmail.com>
2020-09-09 18:48:48 +0300

    Add exercise 459

---
+++ 05-Generative-Recursion/27-Variations-on-the-Theme/Exercise-459.rkt
@@ -0,0 +1,30 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise-459) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; Exercise 459.
+;; Turn the description of the process into an ISL+ function.
+;; Adapt the test cases from figure 165 to this case.
+
+
+(define EPS 0.01)
+
+(define R 159)
+
+
+;; [Number -> Number] Number Number -> Number
+;; Computes the area under the graph of f between a and b.
+;; Assumes that (< a b) holds.
+(check-within (integrate (lambda (x) 20) 12 22) 200 EPS)
+(check-within (integrate (lambda (x) (* 2 x)) 0 10) 100 EPS)
+(check-within (integrate (lambda (x) (* 3 (sqr x))) 0 10) 1000 EPS)
+(define (integrate f a b)
+  (local ((define width (/ (- b a) R))
+          (define mid (+ a (/ width 2)))
+          (define (integrate* i)
+            (cond
+              [(= i R) 0]
+              [else (+ (* width (f (+ mid (* i width))))
+                       (integrate* (add1 i)))])))
+    (integrate* 0)))
+
